07075450b642110a3c54a293acda6f3990014f2b
Rash6 <97577733+Rash6@users.noreply.github.com>
2022-05-29 19:31:07 +0530

    done

---
+++ toggler-closure/index.js
@@ -0,0 +1,17 @@
+
+function toggler(a,b,c) {
+    let counter = document.getElementById("main")
+    var displayCounter = function() {
+        if(Number(counter.innerText)===a){
+            counter.innerText = b;
+        }else if(Number(counter.innerText)===b){
+            counter.innerText = c;
+        }else{
+            counter.innerText=a
+        }
+    };
+    return displayCounter;
+    
+}
+
+const toggle = toggler(1,2,3)
